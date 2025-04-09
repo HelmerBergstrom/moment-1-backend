7044a80a5ef53562b3508310e2f96497f6d61846
Helmer Bergström <hebe2400@student.miun.se>
2025-04-09 15:50:31 +0200

    Fixar generella stilar för hemsidan. Fixar meny och länkarna med "app.get".

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.port || 3001; 
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true }));
+
+// Lägger till vägar till de olika sidorna.
 app.get("/", (req, res) => {
-    res.send("Hello world!");
+    res.render("index");
 });
 
+app.get("/addmember", (req, res) => {
+    res.render("addmember");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+// Startar port och skriver ut till konsol.
 app.listen(port, () => {
     console.log("Server started on: http://localhost:" + port)
 });
